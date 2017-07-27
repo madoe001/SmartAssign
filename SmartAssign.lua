884a678a53ed82124247dd7b288606efe16b16fb
madoe001 <mdoemmecke@gmail.com>
2017-07-27 16:35:16 +0200

    asd
    asdf

--- SmartAssign.lua
+++ SmartAssign.lua
@@ -1,133 +1,67 @@
 --> Mainfuntionality of the Addon
 
 
-function findClassOfPlayer()
-	
-local playerClass = UnitClass("player");
-	
-return playerClass;
-
-end
-
-
-function printGroup()
-   
-   
-	ret = {}
-   
-   
-	if IsInRaid() then
-      
-      
-		local amountMember = GetNumGroupMembers()
-      
-		for i = 1, amountMember do
-         
-         
-			local name = GetUnitName("raid" .. i)
-         
-			ret[name] = UnitClass(name)  
-      
-		end 
-   
-	elseif IsInGroup() then 
-      
-      
-		local amountMember = GetNumSubgroupMembers()
-      
-		ret[GetUnitName("player")] = UnitClass("player")   
-      
-		for i = 1, amountMember do
-         
-         
-			local name = GetUnitName("party" .. i) 
-         
-		ret[name] = UnitClass(name)         
-      
-		end        
-  
-	else
-      
-		print "Player is not in Group "
-   
-	end
-   
-   
-	return ret
-
-end
-
-
 
 function printTable(table)
+   if(table) then
+      if(#table > 0) then
+         for key, value in pairs(table) do
+            print(key, value)
+         end
+      end
+   end
    
-	for key, value in pairs(table) do
-      
-		print(key, value)
-   
-	end
-
 end
-
-
-
-printTable(printGroup())
 
 
 function getAllMembers()
    
-	print (">>getAllMembers()<< called\n");
+   print (">>getAllMembers()<< called\n");
    
- 
-	local raidSize = GetNumGroupMembers() or 0;
- 
-
- 
-   	if(raidSize > 0) then
+   
+   local raidSize = GetNumGroupMembers() or 0;
+   
+   
+   
+   if(raidSize > 0) then
       
-		local playerList = {};
+      local playerList = {};
       
-		if(not IsInRaid() and  IsInGroup) then
-       
-  			raidSize = raidSize -1;
-        
- 			print(GetUnitName("player"));
-  
-			table.insert(playerList, GetUnitName("player)";   
- 		end
-      
-     
- 		for i = 1, raidSize  do
-    
-     			if IsInRaid() then
+      if(not IsInRaid() and  IsInGroup) then
          
-   				print(GetUnitName("raid"..i)); 
-    
-        			table.insert(playerList, GetUnitName("raid"..i));
-      
-      			else if IsInGroup() then
+         raidSize = raidSize -1;
          
-         			print(GetUnitName("party"..i));
+         print(GetUnitName("player"));
+         
+         table.insert(playerList, GetUnitName("player"));   
+      end
       
-           		 	table.insert(playerList, GetUnitName("party"..i));
-       
-           		end           
-                  
-              
- 		end
+      
+      for i = 1, raidSize  do
+         
+         if IsInRaid() then
             
-	end
-           
- 	return  playerList;
+            print(GetUnitName("raid"..i)); 
+            
+            table.insert(playerList, GetUnitName("raid"..i));
+            
+         else if IsInGroup() then
+               
+               print(GetUnitName("party"..i));
+               
+               table.insert(playerList, GetUnitName("party"..i));
+               
+            end           
+            
+            
+         end
+         
+      end
+      
+      return  playerList;
+      
+   end
    
-      	end
-        
- 	return nill;
-      
+   return nil;
+   
 end
-      
-      
-
-
-getAllMembers()
-
