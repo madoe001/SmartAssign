bdb8154bb93c601854c85fa8b9c0e6ee1a1fb60c
madoe001 <mdoemmecke@gmail.com>
2017-07-28 13:47:41 +0200

    cooldown list

---
+++ Cooldown.lua
@@ -0,0 +1,24 @@
+-- ============
+-- CLASS LIST
+-- ============
+
+classList = {
+   ["Warrior"] = {
+      --97462 = "Shout",
+      --118038 = "Die by Swords"
+   }, 
+   ["Death Knight"] = {
+   }, 
+   ["Paladin"] = {
+   },
+   ["Demon Hunter"] = {
+      [196718] = "Darkness"
+   }
+}
+}
+
+function maikSuckt()
+
+	print("TTTT")
+
+end
