ffa727af20ba3751b8f0314e3e053b7ff044a7ef
madoe001 <mdoemmecke@gmail.com>
2017-07-28 14:11:56 +0200

    cd

--- Cooldown.lua
+++ Cooldown.lua
@@ -19,14 +19,4 @@ classList = {
    ["Demon Hunter"] = {
       [196718] = "Darkness"
    }
-<<<<<<< HEAD
 }
-=======
-}
-
-function maikSuckt()
-
-	print("TTTT")
-
-end
->>>>>>> b48ff854285487bf724587f78f5305af61809d31
