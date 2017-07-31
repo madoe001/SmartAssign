7053a3ec7dbf1f1e62624da6cdc314e48d056087
madoe001 <mdoemmecke@gmail.com>
2017-07-31 13:45:39 +0200

    onEvent-tests
    rumspielerei mit onEvents...usw.

--- test.lua
+++ test.lua
@@ -1,1 +1,24 @@
---print("test");
+local startTime = 0
+local endTime = 0
+local totalTime = 0
+local frame = CreateFrame("Frame")
+
+
+function SmartAssign_OnEvent(frame, event, ...)
+   print("OnEvent registered")
+   if event == "PLAYER_REGEN_ENABLED" then
+print("test regen enabled")
+	endTime = GetTime()
+	totalTime = endTime - startTime
+	 if totalTime ~= 0 then
+         print("Totaltime: " .. totalTime)
+      end   
+   elseif event == "PLAYER_REGEN_DISABLED" then
+      startTime = GetTime()
+   end 
+end
+
+frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+frame:RegisterEvent("PLAYER_REGEN_DISABLED")
+
+frame:SetScript("OnEvent", SmartAssign_OnEvent)
