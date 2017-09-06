3f9e1f8bb730448d9c58dc623cc4e664c8ac5395
madoe001 <mdoemmecke@gmail.com>
2017-09-06 17:12:34 +0200

    a
    a

--- test.lua
+++ test.lua
@@ -1,1 +1,19 @@
 --print("test");
+
+TestTable = {
+	var1 = "default 1.0",
+	var2 = "default 2.0",
+	var3 = 3.0,
+	var4 = -5
+}
+
+function printTest()
+	print("var1: " .. TestTable.var1 .. "\nvar2: " .. TestTable.var2 .. "\nvar3: " .. TestTable.var3 .. "\nvar4: " .. TestTable.var4);
+end
+
+function changeVars(v1,v2,v3)
+	TestTable.var1 = v1;
+	TestTable.var2 = v2;
+	TestTable.var3 = v3;
+end
+printTest()
