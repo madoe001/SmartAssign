d670c8d265c5592bcde02f9ade71c3a66a1b268a
madoe001 <mdoemmecke@gmail.com>
2017-09-19 18:59:12 +0200

    asd
    asd

--- GUI/AssignmentFrame.lua
+++ GUI/AssignmentFrame.lua
@@ -8,40 +8,37 @@ do
 	local Assignment = _G.GUI.Assignment
 	
 	function Assignmentframe:new_scrollframe(frame, relativeElement, x, y)
-		
-		local obj = {
+	local obj = {
 			scrollframe = CreateFrame("Scrollframe", nil, frame),   
 			scrollbar = CreateFrame("Slider", nil, scrollframe, "UIPanelScrollBarTemplate")			
 		}
-	
 
-		--init des Frames
-		obj.scrollframe:SetPoint("TOPLEFT",  10, -10)
-		obj.scrollframe:SetPoint("BOTTOMRIGHT",  -10, 10)
-		--obj.scrollframe:SetPoint("BOTTOMLEFT", relativeElement, "BOTTOMRIGHT", 10, 10)
-		local texture = obj.scrollframe:CreateTexture()
-		texture:SetAllPoints()
-		texture:SetTexture(.5,.5,.5,1)
-		frame.scrollframe = scrollframe
-		
-		obj.scrollbar:SetPoint("TOPLEFT", obj.scrollframe, "TOPRIGHT", 4, -16)
-		obj.scrollbar:SetPoint("BOTTOMLEFT", obj.scrollframe, "BOTTOMRIGHT", 4, 16)
-		obj.scrollbar:SetMinMaxValues(1, 200)
-		obj.scrollbar:SetValueStep(1)
-		obj.scrollbar.scrollStep = 1
-		obj.scrollbar:SetValue(0)
+-- Main Test Frame
+		local scrollframe=CreateFrame("ScrollFrame","myFrame",frame)
+		obj.scrollframe:SetBackdrop(StaticPopup1:GetBackdrop())
+--obj.scrollframe:SetSize(300,400)
+		obj.scrollframe:ClearAllPoints()
+		obj.scrollframe:SetPoint("TOPLEFT",relativeElement, "TOPRIGHT" ,0,0)
+		obj.scrollframe:SetPoint( "BOTTOMRIGHT" ,-10, 10)
+		obj.scrollframe:EnableMouse(true)
+		obj.scrollframe:SetMovable(true)
+		obj.scrollframe:RegisterForDrag("LeftButton")
+		obj.scrollframe:SetScript("OnDragStart",scrollframe.StartMoving)
+		obj.scrollframe:SetScript("OnDragStop",scrollframe.StopMovingOrSizing)
+		obj.scrollframe:SetHitRectInsets(10,10,10,10)
+		-- Scroll Bar
+		obj.scrollbar = CreateFrame("Slider","sb",obj.scrollframe,"UIPanelScrollBarTemplate") 
+		obj.scrollbar:SetPoint("TOPLEFT",obj.scrollframe,"TOPRIGHT",5,-20) 
+		obj.scrollbar:SetPoint("BOTTOMLEFT",obj.scrollframe,"BOTTOMRIGHT",5,20) 
+		obj.scrollbar:SetMinMaxValues(1,200) 
+		obj.scrollbar:SetValueStep(1) 
+		obj.scrollbar.scrollStep = 20
+		obj.scrollbar:SetValue(0) 
 		obj.scrollbar:SetWidth(16)
-		obj.scrollbar:SetScript("OnValueChanged", function(self, value)
-			self:GetParent():SetVerticalScroll(value)
-		end)
-		local scrollbg = obj.scrollbar:CreateTexture(nil, "BACKGROUND")
-		scrollbg:SetAllPoints(scrollbar)
-		scrollbg:SetTexture(0,0,0,0.4)
-		frame.scrollbar = scrollbar
-		
-		
-	return obj
-	end
+		obj.scrollbar:SetScript("OnValueChanged",function(self,value) 
+  	    	self:GetParent():SetVerticalScroll(value) 
+end) 
+	end	
 
 		
 	
